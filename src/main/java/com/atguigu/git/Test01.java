66d999385a0374559cd44ba2aba9b6446357889d
gmj <gmj@atguigu.com>
2021-08-14 18:07:12 +0800

    second commit

--- src/main/java/com/atguigu/git/Test01.java
+++ src/main/java/com/atguigu/git/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!");
+        System.out.println("hello Git!!!222");
     }
 }
