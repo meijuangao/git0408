a63ac4a767c0e22e7e02c1f1f5928f14139c68e1
gmj <gmj@atguigu.com>
2021-08-14 18:05:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/Test01.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello Git!!!");
+        System.out.println("hello Git!!!");
+        System.out.println("hello Git!!!");
+    }
+}
